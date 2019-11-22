81e8a5483520a7c8347be9f52a21957528a4076b
Matthew Wang <mwang7@nd.edu>
2019-11-21 20:53:30 -0500

    Changes to code for exercise 2

--- exercise08_question2.R
+++ exercise08_question2.R
@@ -1,15 +1,20 @@
 # Number guessing game between 1 and 100 with 10 attempts
 integer <- 1:100
 magicnumber <- sample(integer,size=1)
+magicnumber <- as.integer(magicnumber)
 print("I'm programmed to generate a random integer between 1 and 100...you have 10 chances.")
 count=0
-while(count<10)
-  if (readline(prompt="Guess:")<magicnumber){
-    count <- count+1
+flag=10
+while(count<flag){
+  value <- as.integer(readline(prompt="Guess:"))
+  if (value<magicnumber){
     print("Higher!")
-  }else if (readline(prompt="Guess:")>magicnumber){
     count <- count+1
+  }else if (value>magicnumber){
     print("Lower!")
+    count <- count+1
   }else{
     print("Correct!")
+    flag=0
   }
+} 
